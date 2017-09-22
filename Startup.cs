c14d243149f2204594ced81bda2a0d284e2acc48
Bri Warrick <30389772+warrickbl@users.noreply.github.com>
2017-09-22 12:47:18 -0400

    update 9-22

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,5 @@
-﻿using bwarrickblog.Models;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin;
-using Microsoft.Owin.Security.Cookies;
+﻿using Microsoft.Owin;
 using Owin;
-using System;
 
 [assembly: OwinStartupAttribute(typeof(bwarrickblog.Startup))]
 namespace bwarrickblog
@@ -14,43 +9,6 @@ namespace bwarrickblog
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-            // Configure the db context and user manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
-            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-
-            // Enable the application to use a cookie to store information for the signed in user
-            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
-            // Configure the sign in cookie
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login"),
-                Provider = new CookieAuthenticationProvider
-                {
-                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                        validateInterval: TimeSpan.FromMinutes(30),
-                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
-                }
-            });
-
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-
-            // Uncomment the following lines to enable logging in with third party login providers
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
-
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
-
-            //app.UseFacebookAuthentication(
-            //   appId: "1464988350261422",
-            //   appSecret: "b0ab7d0a040cd9d4f3888fdd2f0adf96");
-
-            //app.UseGoogleAuthentication(
-            //     clientId: "425199808870-vh0lo5mn66k19v89bve9q0lg9k1eel09.apps.googleusercontent.com",
-            //     clientSecret: "1t1UfWFNADOqM9y1k1-RoMCQ");
         }
     }
 }
